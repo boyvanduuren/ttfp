582e03c61aafe68b3810c4ccbea59fbb51fad18d
B.C. van Duuren <boy@vanduuren.xyz>
2019-08-11 16:02:55 +0200

    Implemented free variables

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,11 +2,12 @@ module Lib
   ( LambdaTerm(..)
   , subterms
   , properSubterm
+  , freeVariables
   ) where
 
 import qualified Data.MultiSet as MS
 
--- | Constructions for Lambda terms, as per TTAFP 1.3.2
+-- | Constructions for Lambda terms, as per TTFP 1.3.2
 data LambdaTerm
   = Var Char
   | Ap LambdaTerm LambdaTerm
@@ -21,16 +22,21 @@ instance Show LambdaTerm where
       Ap t1 t2 -> "(" ++ show t1 ++ ")(" ++ show t2 ++ ")"
       Ab v t1 -> "λ" ++ [v] ++ "." ++ show t1
 
--- | Get the subterms of a LambdaTerm, as per TTAFP 1.3.5
+-- | Get the subterms of a LambdaTerm, as per TTFP 1.3.5
 subterms :: LambdaTerm -> MS.MultiSet LambdaTerm
 subterms x@(Var _) = MS.singleton x
 subterms compoundTerm@(Ap t1 t2) =
   MS.insert compoundTerm $ MS.union (subterms t1) (subterms t2)
 subterms compoundTerm@(Ab _ t2) = MS.insert compoundTerm (subterms t2)
 
--- | Get the proper subterm of a LambaTerm, as per TTAFP 1.3.8
+-- | Get the proper subterm of a LambaTerm, as per TTFP 1.3.8
 -- Note that this functions is dependent on the implementation of `subterms`, as
 -- it expects the first element to be the reflexive element.
 properSubterm :: LambdaTerm -> LambdaTerm -> Bool
 properSubterm l m = subterms l `MS.isProperSubsetOf` subterms m
---freeVariables :: LambaTerm -> [Char]
+
+-- | Get the free variables from a lambda term as defined in TTFP 1.4.1
+freeVariables :: LambdaTerm -> MS.MultiSet Char
+freeVariables (Var x) = MS.singleton x
+freeVariables (Ap t1 t2) = MS.union (freeVariables t1) (freeVariables t2)
+freeVariables (Ab x t) = MS.delete x $ freeVariables t
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,6 +7,7 @@ import Test.HUnit
 
 import qualified Data.MultiSet as MS
 
+-- subterm test cases
 testBasicSubterm =
   assertEqual "Basis for subterms" (MS.singleton $ Var 'x') (subterms varTerm)
   where
@@ -29,11 +30,57 @@ testAbstractionSubterm =
     varTerm = Var 'x'
     abTerm = Ab 'x' varTerm
 
+-- properSubterm test cases
+testProperSubtermNotReflexive =
+  assertEqual
+    "A lambda term is not a proper subterm of itself"
+    False
+    (properSubterm simpleTerm simpleTerm)
+  where
+    simpleTerm = Var 'x'
+
+testProperSubtermSuccess =
+  assertEqual
+    "When MN, N is a proper subterm of M"
+    True
+    (properSubterm termX $ Ap termX termY)
+  where
+    termX = Var 'x'
+    termY = Var 'y'
+
+-- freeVariables test cases
+testVariableFreeVars =
+  assertEqual
+    "A 'Var' lambda term always returns its constructor argument"
+    (MS.singleton 'x')
+    (freeVariables $ Var 'x')
+
+testAbstractionFreeVars =
+  assertEqual
+    "A bound variable can never be free"
+    (MS.empty)
+    (freeVariables $ Ab 'x' $ Var 'x')
+
+testAbstractionOverApplicationFreeVars =
+  assertEqual
+    "Given Lx.xy return ['y']"
+    (MS.singleton 'y')
+    (freeVariables $ Ab 'x' $ Ap (Var 'x') (Var 'y'))
+
 main :: IO ()
 main =
   defaultMainWithOpts
     [ testCase "subterm - basic" testBasicSubterm
     , testCase "subterm - application" testApplicationSubterm
     , testCase "subterm - abstraction" testAbstractionSubterm
+    , testCase
+        "properSubterm - not a proper subterm of itself"
+        testProperSubtermNotReflexive
+    , testCase "properSubterm - success case" testProperSubtermSuccess
+    , testCase "Free variables in 'x' is ['x']" testVariableFreeVars
+    , testCase
+        "A variable bound in an abstraction should not be returned as free"
+        testAbstractionFreeVars
+    , testCase "Given Lx.xy return ['y']" testAbstractionOverApplicationFreeVars
     ]
     mempty
